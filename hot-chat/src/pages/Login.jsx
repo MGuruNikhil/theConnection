39e63c6c5a237eb263600dfaef6f60f43a974ad1
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-01-30 19:39:48 +0530

    basic auth and auth protection for chat completed

--- hot-chat/src/pages/Login.jsx
+++ hot-chat/src/pages/Login.jsx
@@ -34,11 +34,13 @@ const Login = () => {
         <div className="login p-28 bg-[#474B4F] flex flex-col space-y-5 max-w-fit m-auto border-solid border-2 rounded-lg border-[#86C232]">
             <h1>hotCHAT</h1>
             <h2>Log in</h2>
-            <input className='p-2 border-b-2 border-b-[#86C232] focus:outline-none' type="email" name="email" id="email" placeholder='Enter email' />
-            <input className='p-2 border-b-2 border-b-[#86C232] focus:outline-none' type="password" name="password" id="password" placeholder='Enter password' />
-            <a className='cursor-pointer'>Forgot password ?</a>
-            <button>Log in</button>
-            {isErr && <span>{error}</span>}
+            <form onSubmit={handleSubmit} className="flex flex-col space-y-5">
+                <input className='p-2 border-b-2 border-b-[#86C232] focus:outline-none' type="email" name="email" id="email" placeholder='Enter email' />
+                <input className='p-2 border-b-2 border-b-[#86C232] focus:outline-none' type="password" name="password" id="password" placeholder='Enter password' />
+                <a className='cursor-pointer'>Forgot password ?</a>
+                <button>Log in</button>
+                {isErr && <span>{error}</span>}
+            </form>
             <span>don't have an account ? <Link to="/signup">Sign Up</Link></span>
         </div>
     );
