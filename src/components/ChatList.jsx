a325da45147263eae6ade1fd54ba9e3a1aca33f2
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-01-31 22:17:44 +0530

    small styling adjustments

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -4,11 +4,11 @@ import Dummy from '../assets/Dummy.jpg'
 const ChatList = () => {
     return (
         <div className="ChatList flex-1 bg-[#474B4F] h-full overflow-y-scroll scrollbar-hidden">
-            <div className="activeChat max-h-[56px] flex flex-row p-2 justify-between bg-[#222629] border-b-solid border-b-black border-b-2 overflow-hidden">
+            <div className="activeChat max-h-[56px] flex flex-row p-2 justify-between bg-[#222629] border-b-solid border-b-black border-b-2 overflow-hidden cursor-pointer">
                 <img className='rounded-[50%] object-cover' src={Dummy} alt="pp" width={'40px'} height={'40px'} />
                 <p className='self-center flex-1'>Display Name</p>
             </div>
-            <div className="inactiveChat max-h-[56px] flex flex-row p-2 justify-between border-b-solid border-b-black border-b-2 overflow-hidden">
+            <div className="inactiveChat max-h-[56px] flex flex-row p-2 justify-between border-b-solid border-b-black border-b-2 overflow-hidden cursor-pointer">
                 <img className='rounded-[50%] object-cover' src={Dummy} alt="pp" width={'40px'} height={'40px'} />
                 <p className='self-center flex-1'>Display Name</p>
             </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,14 +29,14 @@ const SearchBar = () => {
         <div className="SearchBar">
             <input
                 type="text"
-                className="w-full px-4 py-2 border border-gray-300"
+                className="w-full px-4 py-2 focus:outline-none"
                 style={{ minWidth: '0' }}
                 placeholder="Search..."
                 onChange={(e) => setSearchName(e.target.value)}
                 onKeyDown={handleSubmit}
             />
             {searchResults.map((result, index) => (
-                <div key={index} className="resultItem max-h-[56px] flex flex-row p-2 gap-x-2 border-b-solid border-b-black border-b-2 overflow-hidden">
+                <div key={index} className="resultItem max-h-[56px] flex flex-row p-2 gap-x-2 border-b-solid border-b-black border-b-2 overflow-hidden cursor-pointer">
                     <img className="rounded-[50%] object-cover" src={result.photoURL} alt="pp" width={'40px'} height={'40px'} />
                     <div className="info flex flex-col items-start justify-center">
                         <span className="font-bold text-lg text-left">{result.displayName}</span>
