86d5ae71124f62d3548616c8439bf6433dd5c0dd
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-02-14 16:57:22 +0530

    some icons

--- src/components/SideHeader.jsx
+++ src/components/SideHeader.jsx
@@ -1,20 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
 import { AuthContext } from '../context/AuthContext';
+import logOutIcon from '../assets/logout.png';
 
 const SideHeader = () => {
-    const {currentUser} = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
     const displayName = currentUser.displayName;
     const photoURL = currentUser.photoURL;
 
+    const handleLogout = () => {
+        const confirmLogout = window.confirm('Are you sure you want to log out ?');
+        if (confirmLogout) {
+            signOut(auth)
+            .then(() => {
+                console.log('User logged out successfully');
+            })
+            .catch((error) => {
+                console.error('Error during logout:', error);
+            });
+        }
+    };
+
     return (
         <div className="SideHeader max-h-[56px] flex flex-row p-2 justify-between bg-[#61892F] overflow-hidden">
-            <img src={photoURL} alt="pp" width={'40px'} height={'40px'} className='rounded-[50%] object-cover'/>
+            <img src={photoURL} alt="pp" width={'40px'} height={'40px'} className='rounded-[50%] object-cover hover:border-[1px] hover:border-solid hover:border-[#86C232] cursor-pointer' />
             <p className='self-center'>{displayName}</p>
-            <button onClick={()=>signOut(auth)} className='h-[40px] px-1 py-2 text-center'>Log Out</button>
+            <button onClick={handleLogout} className='h-[40px] w-[40px] px-1 py-2 bg-inherit rounded-full focus:outline-none flex items-center justify-center'>
+                <img src={logOutIcon} width={20} height={20} alt="log out" />
+            </button>
         </div>
     );
-}
+};
 
 export default SideHeader;
