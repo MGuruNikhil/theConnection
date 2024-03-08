c85f5c3079db5899387d5bbcde614dc74b98429f
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-08 21:10:47 +0530

    removed ring

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -26,7 +26,7 @@ const Send = () => {
         <div className="Send w-full flex flex-row bg-[#000000]">
             <input
                 type="text"
-                className="flex-1 px-4 py-2 border border-gray-300 rounded-lg"
+                className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none"
                 style={{ minWidth: '0' }}
                 placeholder='Type here...'
                 value={message}
