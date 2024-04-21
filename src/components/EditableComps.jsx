5b9a3881c6940618032f5019d11862524feb3377
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-04-21 11:24:35 +0530

    edit ui

--- src/components/EditableComps.jsx
+++ src/components/EditableComps.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Edit from "../assets/edit.png"
 import Close from "../assets/close.png"
 
@@ -7,13 +7,37 @@ const EditableComp = (props) => {
 
     const [isEdit, setIsEdit] = useState(false);
 
+    const [editedText, setEditedText] = useState((props.value != '-')?props.value:'');
+
+    const inputRef = useRef(null);
+
+    const handleClick = () => {
+        setIsEdit(prev =>!prev);
+    };
+
+    useEffect(() => {
+        if (isEdit && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isEdit]);
 
     return (
         <div className="flex items-center justify-between w-[70%] p-2 h-[50px] gap-2">
             <p className="flex-shrink-0 inline-block whitespace-no-wrap text-[#ffffff] text-semibold">{props.label} :</p>
-            {!isEdit ? <span className="text-[#86C232] text-bold">{props.value}</span> : <input style={{ minWidth: '0' }} className="flex-grow rounded-md p-[2px] focus:outline-none focus:ring-2 focus:ring-[#86C232] h-[100%]" type="text" name={props.key} id={props.key} /> }
-            <button onClick={() => {setIsEdit(prev=>!prev)}}>
-                <img src={!isEdit ? Edit : Close} width={20} height={20} alt="edit" />
+            {!isEdit? <span className="text-[#86C232] text-bold">{props.value}</span> : 
+                <input 
+                    ref={inputRef} 
+                    style={{ minWidth: '0' }} 
+                    onChange={(e)=>{setEditedText(e.target.value)}} 
+                    value={editedText} 
+                    className="flex-grow rounded-md p-[2px] focus:outline-none focus:ring-2 focus:ring-[#86C232] h-[100%]" 
+                    type="text" 
+                    name={props.fbkey} 
+                    id={props.fbkey} 
+                /> 
+            }
+            <button onClick={handleClick}>
+                <img src={!isEdit? Edit : Close} width={20} height={20} alt="edit" />
             </button>
         </div>
     )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,9 +27,9 @@ const Profile = () => {
     return (
         <div className="p-16 bg-[#474B4F] flex flex-col items-center w-[50%] h-full m-auto border-solid border-2 rounded-lg border-[#86C232]">
             <img src={currentUser.photoURL} className='rounded-full w-[200px] h-[200px] object-cover'  alt="pp" />
-            <EditableComp label="Display Name" value={currentUser.displayName} key="displayName" />
-            <EditableComp label="Email id" value={currentUser.email} key="email" />
-            <EditableComp label="Phone Number" value={currentUser.phoneNumber||"-"} key="phoneNumber" />
+            <EditableComp label="Display Name" value={currentUser.displayName} fbkey="displayName" />
+            <EditableComp label="Email id" value={currentUser.email} fbkey="email" />
+            <EditableComp label="Phone Number" value={currentUser.phoneNumber||"-"} fbkey="phoneNumber" />
             <div className="flex flex-row gap-2">
                 <button onClick={handleLogout} className='gap-2 border border-transparent text-base font-semibold font-inherit cursor-pointer transition-border-color duration-250 overflow-hidden text-[#86C232] focus-visible:ring-4 focus-visible:ring-auto focus-visible:ring-[#86C232] hover:border-[#86C232] h-[40px] px-2 py-4 bg-inherit rounded-full focus:outline-none flex items-center justify-center'>
                     <img src={logOutIcon} width={20} height={20} alt="log out" />
