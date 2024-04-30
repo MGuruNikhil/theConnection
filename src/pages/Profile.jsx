06d490183e03c9e2d5cff626175e6bafaf084c59
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-04-30 15:05:11 +0530

    profile pic removel successful

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import logOutIcon from '../assets/logout.png';
 import { auth, storage, db } from "../firebase";
 import { signOut, updateProfile } from "firebase/auth";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
 import { doc, updateDoc } from "firebase/firestore";
 import EditableComp from "../components/EditableComps";
 import Back from "../assets/arrow.png";
 import Edit from "../assets/edit.png";
+import Close from "../assets/close.png"
 import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
 
     const { currentUser } = useContext(AuthContext);
-    
+
+    const [showButton, setShowButton] = useState(false);
+
     const navigate = useNavigate();
 
     console.log(currentUser);
@@ -33,35 +36,60 @@ const Profile = () => {
 
     const handleEditPP = (e) => {
         const photo = e.target.files[0];
-        if(photo) {
+        if (photo) {
             const storageRef = ref(storage, 'profilePics/' + currentUser.uid + '.jpg');
             const uploadTask = uploadBytesResumable(storageRef, photo);
             uploadTask.on(
                 (error) => {
-                    setIsErr(true);
                     const errorCode = error.code;
                     const errorMessage = error.message;
-                    console.log(errorMessage);
-                    setError(errorMessage);
+                    console.log(errorMessage); 
                 },
                 () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
                         await updateProfile(currentUser, {
                             photoURL: downloadURL,
                         }).catch((error) => {
-                            setIsErr(true);
                             const errorCode = error.code;
                             const errorMessage = error.message;
                             console.log(errorMessage);
-                            setError(errorMessage);
                         });
                         await updateDoc(doc(db, "users", currentUser.uid), {
                             photoURL: downloadURL,
-                        })
+                        }).catch((error) => {
+                            const errorCode = error.code;
+                            const errorMessage = error.message;
+                            console.log(errorMessage);
+                        });
                     });
                 }
             );
+            setShowButton(false);
         }
+    }
+
+    const handleRemovePP = async () => {
+        deleteObject(ref(storage, 'profilePics/'+currentUser.uid+'.jpg'))
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+        await updateProfile(currentUser, {
+            photoURL: "https://firebasestorage.googleapis.com/v0/b/hotchat-nik.appspot.com/o/profilePics%2FDummy.png?alt=media&token=a39fc600-99f7-490d-a670-c23dc37e8d53",
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+        await updateDoc(doc(db, "users", currentUser.uid), {
+            photoURL: "https://firebasestorage.googleapis.com/v0/b/hotchat-nik.appspot.com/o/profilePics%2FDummy.png?alt=media&token=a39fc600-99f7-490d-a670-c23dc37e8d53",
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+        setShowButton(false);
     }
 
     return (
@@ -70,11 +98,22 @@ const Profile = () => {
             <button onClick={() => { navigate("/") }} className="absolute top-2 left-2 z-10 bg-[#86C232] rounded-full cursor-pointer p-2 flex items-center justify-center"><img src={Back} width={20} height={20} alt="log out" /></button>
 
             <div className="relative">
-                <img src={currentUser.photoURL} className='rounded-full w-[200px] h-[200px] object-cover' alt="pp" />
-                <label for="ppUpload" className="absolute bottom-2 right-2 z-10 bg-[#86C232] rounded-full cursor-pointer p-2 flex items-center justify-center">
-                    <img src={Edit} width={30} height={30} alt="edit" />
-                </label>
+
+                <img src={currentUser.photoURL} className='rounded-full w-[200px] h-[200px] object-cover border-2 border-solid border-[#86C232]' alt="pp" />
+
+                <div className="bg-[#474B4F] absolute bottom-2 right-2 z-10 flex gap-2 rounded-full hover:border-2 hover:border-solid hover:border-[#86C232]" onMouseEnter={() => setShowButton(true)} onMouseLeave={() => setShowButton(false)}>
+                    {showButton && (
+                        <button onClick={handleRemovePP} className="bg-[#6B6E70] rounded-full cursor-pointer p-2 flex items-center justify-center">
+                            <img src={Close} width={30} height={30} alt="remove profile picture" />
+                        </button>
+                    )}
+                    <label for="ppUpload" className="bg-[#86C232] rounded-full cursor-pointer p-2 flex items-center justify-center">
+                        <img src={Edit} width={30} height={30} alt="edit" />
+                    </label>
+                </div>
+
                 <input className="hidden" type="file" accept="image/*" id="ppUpload" onChange={handleEditPP} />
+
             </div>
 
             <EditableComp label="Display Name" fbkey="displayName" />
