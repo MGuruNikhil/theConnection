5c847bd8cf3d1da08495ba3e956a473200aadb2c
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-02 13:35:40 +0530

    okay

--- src/components/CACover.jsx
+++ src/components/CACover.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext';
 
 const CACover = () => {
-    const {currentUser} = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
+    
     return (
         <div className='w-full h-full flex flex-col items-center justify-center'>
-            <img src={currentUser.photoURL} className='rounded-[50%] w-[200px] h-[200px] object-cover'  alt="pp" />
+            <img src={currentUser.photoURL} className='rounded-[50%] w-[200px] h-[200px] object-cover' alt="pp" />
             <p className='text-3xl'>Hay <span className='font-bold text-[#86C232]'>{currentUser.displayName}</span></p>
             <p className='text-sm'>You can search for other people in the app to chat</p>
             <p className='text-xs'>Just try it out using search bar</p>
