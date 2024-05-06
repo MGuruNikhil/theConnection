9b67fa20a487c939f07185f101c229df813dd124
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-06 16:04:06 +0530

    every thing is responsive finally

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react'
 import { ChatContext } from '../context/ChatContext';
+import Back from '../assets/arrow.png'
 
 const ChatHeader = () => {
-    const {otherUser} = useContext(ChatContext);
+    const {otherUser, setOtherUser} = useContext(ChatContext);
     return (
         <div className="ChatHeader h-[56px] w-full flex flex-row p-2 bg-[#61892F]">
-            <img src={otherUser?.photoURL} width={'40px'} height={'40px'} alt="pp" className='rounded-[50%] object-cover' />
+            <div className='flex gap-2'>
+                <button onClick={() => { setOtherUser(null) }} className="cursor-pointer p-2 flex items-center justify-center"><img src={Back} width={20} height={20} alt="back" /></button>
+                <img src={otherUser?.photoURL} width={'40px'} height={'40px'} alt="pp" className='rounded-[50%] object-cover' />
+            </div>
             <p className='flex-1 self-center'>{otherUser?.displayName}</p>
         </div>
     );
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { Component, useContext } from 'react';
 import Sidebar from '../components/Sidebar';
 import ChatArea from '../components/ChatArea';
+import { ChatContext } from '../context/ChatContext';
 
-const Chat = () => {
-    return (
-        <div className="container min-w-[768px] w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid flex flex-row">
-            <Sidebar />
-            <ChatArea />
-        </div>
-    );
+class Chat extends Component {
+
+    static contextType = ChatContext;
+
+    constructor(props) {
+        super(props)
+        this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+    }
+
+    componentDidMount() {
+        const handler = e => this.setState({ matches: e.matches });
+        window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
+    }
+
+    render() {
+
+        const { otherUser } = this.context;
+
+        return (
+            <>
+                {this.state.matches && (
+                    <div className="container min-w-[768px] w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid flex flex-row">
+                        <Sidebar />
+                        <ChatArea />
+                    </div>
+                )}
+                {(!this.state.matches && otherUser==null) && (
+                    <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
+                        <Sidebar />
+                    </div>                
+                )}
+                {(!this.state.matches && otherUser!=null) && (
+                    <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
+                        <ChatArea />
+                    </div>
+                )}
+            </>
+        );
+    }
 }
 
 export default Chat;
