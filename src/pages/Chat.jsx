9ef354e4909c6f75c4a52e309164236aa5346121
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-18 16:58:41 +0530

    removed class

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,47 +1,38 @@
-import React, { Component, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import ChatArea from '../components/ChatArea';
 import { ChatContext } from '../context/ChatContext';
 
-class Chat extends Component {
+const Chat = () => {
 
-    static contextType = ChatContext;
+    const { otherUser } = useContext(ChatContext);
+    const [matches, setMatches] = useState(window.matchMedia("(min-width: 768px)").matches)
 
-    constructor(props) {
-        super(props)
-        this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
-    }
-
-    componentDidMount() {
-        const handler = e => this.setState({ matches: e.matches });
+    useEffect(() => {
+        const handler = e => setMatches(e.matches);
         window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
-    }
+    },[]);
 
-    render() {
-
-        const { otherUser } = this.context;
-
-        return (
-            <>
-                {this.state.matches && (
-                    <div className="container min-w-[768px] w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid flex flex-row">
-                        <Sidebar />
-                        <ChatArea />
-                    </div>
-                )}
-                {(!this.state.matches && otherUser==null) && (
-                    <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
-                        <Sidebar />
-                    </div>                
-                )}
-                {(!this.state.matches && otherUser!=null) && (
-                    <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
-                        <ChatArea />
-                    </div>
-                )}
-            </>
-        );
-    }
+    return (
+        <>
+            {matches && (
+                <div className="container min-w-[768px] w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid flex flex-row">
+                    <Sidebar />
+                    <ChatArea />
+                </div>
+            )}
+            {(!matches && otherUser==null) && (
+                <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
+                    <Sidebar />
+                </div>                
+            )}
+            {(!matches && otherUser!=null) && (
+                <div className="container w-[80%] h-full m-auto self-center rounded-lg overflow-hidden border-2 border-[#86C232] border-solid">
+                    <ChatArea />
+                </div>
+            )}
+        </>
+    );
 }
 
 export default Chat;
