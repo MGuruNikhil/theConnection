d713be6f86f14dda026a0b0637dc0df989222bc9
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-19 20:49:37 +0530

    initial scrol bottom bug solved

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
 import { doc, onSnapshot } from 'firebase/firestore';
@@ -9,6 +9,7 @@ const Messages = () => {
     const {otherUser} = useContext(ChatContext);
     const ChatID = (currentUser.uid < otherUser?.uid)?(currentUser.uid+"-"+otherUser?.uid):(otherUser?.uid+"-"+currentUser.uid);
     const [chats, setChats] = useState([]);
+    const divRef = useRef(null);
 
     useEffect(() => {
         const renderMessages = () => {
@@ -22,8 +23,15 @@ const Messages = () => {
         otherUser?.uid && renderMessages();
     }, [otherUser?.uid]);
 
+    useEffect(() => {
+        const div = divRef.current;
+        if (div) {
+          div.scrollTop = div.scrollHeight;
+        }
+    }, [chats]);
+
     return (
-        <div className="Messages flex-1 bg-gray-950 flex flex-col gap-2 overflow-scroll p-2 scrollbar-hidden">
+        <div ref={divRef} className="Messages flex-1 bg-gray-950 flex flex-col gap-2 overflow-scroll p-2 scrollbar-hidden">
             {chats && chats.map((chat, index) => (  
                 <div key={index} className={`break-words bg-[#86C232] max-w-[90%] ${chat.from === currentUser.uid ? 'place-self-end rounded-tr-none' : 'place-self-start rounded-tl-none'} px-4 py-2 rounded-xl text-start`}>{chat.message}</div>
             ))}
