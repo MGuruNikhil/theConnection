8e06361189a28e36685d12561e77b776d0cc5e22
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-06-23 12:49:58 +0530

    img stop propogation

--- src/components/MyAvatar.jsx
+++ src/components/MyAvatar.jsx
@@ -6,7 +6,7 @@ const MyAvatar = (props) => {
 
     const { setIsActive, setImgUrl } = useContext(ImgContext);
     return (
-        <div className={`flex items-center justify-center max-w-[${props.width}] max-h-[${props.height}] min-w-[${props.width}] min-h-[${props.height}]`}>
+        <div onClick={(e) => e.stopPropagation()} className={`flex items-center justify-center max-w-[${props.width}] max-h-[${props.height}] min-w-[${props.width}] min-h-[${props.height}]`}>
             <Avatar onClick={() => {setImgUrl(props.src); setIsActive(true);}} src={props.src} sx={{ width: props.width, height: props.height }} alt='profile picture' className={`${props.className} cursor-pointer hover:border-2 hover:border-solid hover:border-[#9d1919]`} />
         </div>
     )
