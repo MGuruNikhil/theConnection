591495c87108d26f1819cf9404b18fd27673ce10
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-06-25 19:03:49 +0530

    fullImg, click outside->go back

--- src/components/FullImg.jsx
+++ src/components/FullImg.jsx
@@ -7,9 +7,9 @@ const FullImg = () => {
     const { imgUrl, isActive, setIsActive } = useContext(ImgContext);
 
     return (
-        <div className={`${isActive ? 'flex' : 'hidden'} w-full h-full items-center justify-center bg-inherit backdrop-blur-sm z-50 fixed top-0 right-0 left-0 bottom-0`}>
-            <button onClick={() => {setIsActive(false);}} className='fixed top-2 right-2'><CloseIcon /></button>
-            <img className='max-w-[90%] max-h-[90%]' src={imgUrl} alt="profile pic" />          
+        <div onClick={() => {setIsActive(false);}} className={`${isActive ? 'flex' : 'hidden'} w-full h-full items-center justify-center bg-inherit backdrop-blur-sm z-50 fixed top-0 right-0 left-0 bottom-0`}>
+            <button onClick={() => {setIsActive(false);}} className='fixed top-2 right-2 z-50'><CloseIcon /></button>
+            <img onClick={(e) => e.stopPropagation()} className='max-w-[90%] max-h-[90%]' src={imgUrl} alt="profile pic" />          
         </div>
     )
 }
